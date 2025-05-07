f1c3cbf21ce2701ca5d0ffc656cf396475922067
arturo <arturo@gmail.com>
2025-05-07 17:41:06 -0400

    cambios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const students=[];
 
-document.getElementById("studentForm").addEventListener("submint",function(e){
+document.getElementById("studentForm").addEventListener("submit",function(e){
 e.preventDefault();
 
 const name=document.getElementById("name").value.trim();
 const lastName=document.getElementById("lastName").value.trim();
 const grade=parseFloat(document.getElementById("grade").value);
 
-if(!name || !lastName || isNaN(grade)|| grade<1 || grade<7){
+if(!name || !lastName || isNaN(grade)|| grade<1 || grade>7){
     alert("Error al ingresar los datos")
     return
 }
